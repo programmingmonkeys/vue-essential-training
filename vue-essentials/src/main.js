cfe4f5f46ea8bf31779b58cefb9fc7d0b2b8ee83
codingChewie <codingChewie@gmail.com>
2020-10-20 09:53:58 -0500

    Installing additional modules

--- vue-essentials/src/main.js
+++ vue-essentials/src/main.js
@@ -1,10 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'bootstrap'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-Vue.config.productionTip = false;
+import 'bootstrap/dist/css/bootstrap.css'
+import 'animate.css/animate.css'
+
+import { faShoppingCart, faDollarSign } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faShoppingCart, faDollarSign)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app')
