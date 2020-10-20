14ef69b9cdcfab029ec82bdd4efa10290105c73f
codingChewie <codingChewie@gmail.com>
2020-10-20 10:06:24 -0500

    Testing your module installs

--- vue-essentials/src/App.vue
+++ vue-essentials/src/App.vue
@@ -1,32 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div id="app" class="container mt-5">
+    <h1>My Shop</h1>
+    <p class="animated fadeInRight">Take a look at our offerings below</p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "app",
+  components: {
+    FontAwesomeIcon
+  }
+};
+</script>
--- vue-essentials/src/main.js
+++ vue-essentials/src/main.js
@@ -1,19 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'bootstrap'
-import { library } from '@fortawesome/fontawesome-svg-core'
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'animate.css/animate.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "animate.css/animate.css";
 
-import { faShoppingCart, faDollarSign } from '@fortawesome/free-solid-svg-icons'
+import {
+  faShoppingCart,
+  faDollarSign
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faShoppingCart, faDollarSign)
+library.add(faShoppingCart, faDollarSign);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
